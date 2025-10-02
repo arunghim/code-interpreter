b419a0f2561514a5b55a758b759f87c6bb41f4f3
Arun Ghimire <dev.arunghimire@gmail.com>
2025-10-02 16:04:49 -0400

    Initial Commit: Project Setup

---
+++ src/Tokenizer.java
@@ -0,0 +1,26 @@
+public class Tokenizer {
+
+    public Tokenizer(String path) {
+
+    }
+
+    public int getToken() {
+        return 0;
+    }
+
+    public void skipToken() {
+
+    }
+
+    public int intVal() {
+        return 0;
+    }
+
+    public String idName() {
+        return "";
+    }
+
+    public String currentToken() {
+        return "";
+    }
+}
